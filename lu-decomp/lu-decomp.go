596305031511061039ff8366ceb77e88bc7c7cd5
awmottaz <anthonywmottaz@gmail.com>
2016-10-07 13:21:26 -0500

    Update lu-decomp

--- lu-decomp/lu-decomp.go
+++ lu-decomp/lu-decomp.go
@@ -14,11 +14,17 @@ import (
 	"github.com/nsf/termbox-go"
 )
 
-var in = bufio.NewReader(os.Stdin)
+var inputBuffer = bufio.NewReader(os.Stdin)
+var instructions = "Find the LU-Factorization of a matrix A\n\n" +
+	"Please enter your matrix A.\n" +
+	"This is accomplished by entering one row at a time, separating numbers with a space. " +
+	"When finished with a row, hit the [RETURN] key. " +
+	"This program takes only square matrices for A. " +
+	"It will count the number of elements in row 1 and automatically " +
+	"stop taking input after you have entered that many rows. " +
+	"For example:\n\n row 1: 1 2 3 [RETURN] \n row 2: 4 5 6 [RETURN] \n row 3: 7 8 9 [RETURN]\n\n"
 
 func main() {
-	fmt.Println("Find the L-U decomposition of a matrix A.\n")
-
 	// Get terminal width
 	if err := termbox.Init(); err != nil {
 		panic(err)
@@ -27,42 +33,35 @@ func main() {
 	termbox.Close()
 	width := uint(math.Min(float64(w), float64(80)))
 
-	instructions := "Please enter your matrix A.\n" +
-		"This is accomplished by entering one row at a time, separating numbers with a space. " +
-		"When finished with a row, hit the [RETURN] key. " +
-		"This program takes only square matrices for A. " +
-		"It will count the number of elements in row 1 and automatically " +
-		"stop taking input after you have entered that many rows. " +
-		"For example:\n\n row 1: 1 2 3 [RETURN] \n row 2: 4 5 6 [RETURN] \n row 3: 7 8 9 [RETURN]\n\n"
+	// Print the instructions for the user
 	fmt.Print(wordwrap.WrapString(instructions, width))
 
+	// Read the first row and calculate its size n. A will be an n x n matrix.
 	fmt.Print("row 1: ")
-	row, _ := in.ReadString('\n')
-	r1 := strings.Fields(row)
-	n := len(r1)
+	rowString, _ := inputBuffer.ReadString('\n')
+	r1Array := strings.Fields(rowString)
+	n := len(r1Array)
 
-	// Initialize matrix A
+	// Initialize matrices A and LU
 	A := make([][]float64, n)
 	LU := Zeros(n)
 
-	// Add the first row
-	for _, num := range r1 {
-		if val, e := strconv.ParseFloat(num, 64); e == nil {
-			A[0] = append(A[0], val)
+	// Add the first row to A
+	for _, elementString := range r1Array {
+		if elementFloat, err := strconv.ParseFloat(elementString, 64); err == nil {
+			A[0] = append(A[0], elementFloat)
 		}
 	}
 	// Now add the rest
 	for i := 1; i < n; i++ {
-		fmt.Print("row ")
-		fmt.Print(i + 1)
-		fmt.Print(": ")
-		row, _ = in.ReadString('\n')
-		if len(strings.Fields(row)) != n {
+		fmt.Print("row ", i+1, ": ")
+		rowString, _ = inputBuffer.ReadString('\n')
+		if len(strings.Fields(rowString)) != n {
 			panic("Invalid row! Quitting...")
 		}
-		for _, num := range strings.Fields(row) {
-			if val, e := strconv.ParseFloat(num, 64); e == nil {
-				A[i] = append(A[i], val)
+		for _, elementString := range strings.Fields(rowString) {
+			if elementFloat, err := strconv.ParseFloat(elementString, 64); err == nil {
+				A[i] = append(A[i], elementFloat)
 			}
 		}
 	}
@@ -70,35 +69,39 @@ func main() {
 	fmt.Println()
 
 	// Show the user what they entered for A
-	PrintArray(A, n, "A") // PrintArray(arr, size, name)
+	PrintArray(A, n, "A")
 
+	// If the calculation takes a long time, the spinner will indicate
+	// that the program is still running
 	s := spinner.New(spinner.CharSets[14], 100*time.Millisecond)
 	s.Suffix = " Calculating decomposition..."
 	s.Start()
 
-	for row, data := range A {
-		for col, elem := range data {
+	// Calculate LU
+	for rowIndex, rowData := range A {
+		for colIndex, element := range rowData {
 			var sum float64
 			var newVal float64
-			if row <= col {
-				for i := 0; i < row; i++ {
-					sum += LU[row][i] * LU[i][col]
+			if rowIndex <= colIndex {
+				for k := 0; k < rowIndex; k++ {
+					sum += LU[rowIndex][k] * LU[k][colIndex]
 				}
-				newVal = elem - sum
+				newVal = element - sum
 			} else {
-				for i := 0; i < col; i++ {
-					sum += LU[row][i] * LU[i][col]
+				for k := 0; k < colIndex; k++ {
+					sum += LU[rowIndex][k] * LU[k][colIndex]
 				}
-				newVal = (elem - sum) / LU[col][col]
+				newVal = (element - sum) / LU[colIndex][colIndex]
 			}
-			LU[row][col] = newVal
+			LU[rowIndex][colIndex] = newVal
 		}
 	}
 
 	s.Stop()
 
-	fmt.Println("The resulting L-U decomposition is combined into a single array:\n")
-	PrintArray(LU, n, "L-U")
+	// Print the answer on the screen
+	fmt.Println("The resulting LU-factorization is combined into a single array:\n")
+	PrintArray(LU, n, "LU")
 }
 
 func Zeros(size int) [][]float64 {
@@ -112,14 +115,14 @@ func Zeros(size int) [][]float64 {
 }
 
 func PrintArray(arr [][]float64, size int, name string) {
-	for ind, mrow := range arr {
-		if math.Ceil(float64(size)/float64(2)) == float64(ind+1) {
+	for rowIndex, rowData := range arr {
+		if math.Ceil(float64(size)/float64(2)) == float64(rowIndex+1) {
 			fmt.Print(name, " =\t")
 		} else {
 			fmt.Print("\t")
 		}
-		for _, elem := range mrow {
-			fmt.Print(elem, "\t")
+		for _, element := range rowData {
+			fmt.Print(element, "\t")
 		}
 		fmt.Println()
 	}
