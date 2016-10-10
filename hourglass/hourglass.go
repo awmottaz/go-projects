462cb57f5f25602c42fc875e038112a2d523ed6f
awmottaz <anthonywmottaz@gmail.com>
2016-10-10 08:48:59 -0500

    Complete hourglass program

--- hourglass/hourglass.go
+++ hourglass/hourglass.go
@@ -3,15 +3,17 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
 var scanner = bufio.NewScanner(os.Stdin)
-var maxHourglassSum int
 
 const arraySize = 6
+
+var maxHourglassSum = int(-9 * arraySize * arraySize)
 
 var A [arraySize][arraySize]int
 
@@ -19,12 +21,11 @@ func main() {
 	//Enter your code here. Read input from STDIN. Print output to STDOUT
 	readInput()
 	hourglasses := fetchHourglasses()
-	fmt.Print(hourglasses)
-	/*
-	for _, currentHourglass := range(hourglasses) {
-		maxHourglassSum = int( math.Max( float64(maxHourglassSum), float64(calculateHourglassSum(currentHourglass)) ) )
+	for _, currentHourglass := range hourglasses {
+		currentHourglassSum := calculateHourglassSum(currentHourglass)
+		maxHourglassSum = int(math.Max(float64(maxHourglassSum), float64(currentHourglassSum)))
 	}
-	*/
+	fmt.Print(maxHourglassSum)
 }
 
 func readInput() {
@@ -39,12 +40,23 @@ func readInput() {
 	}
 }
 
-func fetchHourglasses() [][][]int {
-	hourglasses := make([][][]int, arraySize)
-	for i := 0; i < arraySize - 2; i++ {
-		for j := 0; j < arraySize - 2; j++ {
-			hourglasses[i][j] = [ A[i][j] A[i+1][j] A[i+2][j] A[i+1][j+1] A[i][j+2] A[i+1][j+2] A[i+2][j+2] ]
+func fetchHourglasses() [][]int {
+	numHourglasses := (arraySize - 2) * (arraySize - 2)
+	hourglasses := make([][]int, numHourglasses)
+	counter := 0
+	for i := 0; i < arraySize-2; i++ {
+		for j := 0; j < arraySize-2; j++ {
+			hourglasses[counter] = []int{A[i][j], A[i][j+1], A[i][j+2], A[i+1][j+1], A[i+2][j], A[i+2][j+1], A[i+2][j+2]}
+			counter++
 		}
 	}
 	return hourglasses
+}
+
+func calculateHourglassSum(hourglass []int) int {
+	sum := 0
+	for _, element := range hourglass {
+		sum += element
+	}
+	return sum
 }
